ce0e015ccd4f5e0560ca4099d116616d9cbab829
Melquisedeque Pereira <melqui1@live.com.pt>
2020-10-31 20:46:53 +0000

    Rebuild

--- src/generator.js
+++ src/generator.js
@@ -78,17 +78,15 @@ function writeToOutput(result) {
         <html>
             <head>
                 <title>GraphQL Docs</title>
-                <meta charset="utf-8">
+                <meta charset='utf-8'>
+                <script defer src='https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react.js'></script>
+                <script defer src='https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react-dom.js'></script>
+                <script defer src='https://cdnjs.cloudflare.com/ajax/libs/fetch/1.0.0/fetch.min.js'></script>
+                <script defer>${distScript}</script>
             </head>
-
             <body>
-                <div id="app"></div>
-
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.2/react-dom.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/1.0.0/fetch.min.js"></script>
-                <script>${distScript}</script>
-                <script>
+                <div id='app'></div>
+                <script defer>
                     function fetcher() {
                         return new Promise(function(resolve) {
                             resolve(${JSON.stringify(result)});
